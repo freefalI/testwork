2e66a605f45523ff71bfc3068d2aa12379df3f3b
freefalI <andriyshmatko1999@gmail.com>
2020-07-08 16:05:37 +0300

    refactor database.php config

--- config/database.php
+++ config/database.php
@@ -90,29 +90,14 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-        /*'mongodb' => [
-            'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'homestead'),
-            'username' => env('DB_USERNAME', 'homestead'),
-            'password' => env('DB_PASSWORD', 'secret'),
-            'options' => [
-                // here you can pass more settings to the Mongo Driver Manager
-                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
-
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-            ],
-
-        ],*/
         'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => 'mongo',
-            'port'     => 27017,
-            'database' => 'database',
-            'username' => '',
-            'password' => '',
-            'options'  => [
+            'driver' => 'mongodb',
+            'host' => env('MONGODB_HOST', 'mongo'),
+            'port' => env('MONGODB_PORT', 27017),
+            'database' => env('MONGODB_DATABASE', 'database'),
+            'username' => env('MONGODB_USERNAME', ''),
+            'password' => env('MONGODB_PASSWORD', ''),
+            'options' => [
                 'database' => '',
             ]
         ],
